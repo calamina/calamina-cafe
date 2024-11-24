1197c374a91114ff19b0a853182683c370a8a7f7
calamina <gbelliere@gmail.com>
2024-11-24 11:47:03 +0100

    remove navigation images :'(

--- app/components/content/project/ProjectInfo.vue
+++ app/components/content/project/ProjectInfo.vue
@@ -9,19 +9,20 @@ const { name, url = null, tech } = defineProps<{
 </script>
 
 <template>
-  <div class="tw-flex tw-flex-col tw-gap-1 xl:tw-w-1/3 xl:tw-fixed tw-px-4 xl:tw-px-8  xl:tw-pt-16 tw-pb-4 xl:tw-top-0 xl:tw-h-full xl:tw-left-0">
+  <div
+    class="tw-flex tw-flex-col tw-gap-1 xl:tw-w-1/3 xl:tw-fixed tw-px-4 xl:tw-px-8  xl:tw-pt-16 tw-pb-4 xl:tw-top-0 xl:tw-h-full xl:tw-left-0">
     <!-- <div class="tw-flex tw-pt-8 tw-pb-8">
       <p class="lel">✦</p>
       <p class="lel">✦</p>
       <p class="lel">✦</p>
     </div> -->
-    <div class="tw-flex tw-pt-6 tw-pb-6">
-      <p class=" tw-text-3xl">✦</p>
-      <p class=" tw-text-3xl">✦</p>
-      <p class=" tw-text-3xl">✦</p>
+    <div class="stars tw-flex tw-pt-6 tw-pb-6">
+      <p class="star tw-text-3xl">✦</p>
+      <p class="star tw-text-3xl">✦</p>
+      <p class="star tw-text-3xl">✦</p>
     </div>
     <ProjectName :name />
-    <ProjectLink v-if="url":url="url" />
+    <ProjectLink v-if="url" :url="url" />
     <slot />
     <ProjectTech :tech />
   </div>
@@ -45,5 +46,50 @@ const { name, url = null, tech } = defineProps<{
 :slotted(p) {
   padding: 1.5rem 0;
   text-justify: distribute;
+}
+
+.star {
+  display: flex;
+  // align-items: center;
+  justify-content: center;
+  transform-origin: 50% 50%;
+  width: fit-content; 
+  // SMOL
+  height: 20px;
+  width: 20px;
+  line-height: 22px;
+  // BIG
+  height: 20px;
+  width: 20px;
+  line-height: 26px;
+}
+
+.stars :nth-child(1) {
+  animation: rotate 6s  cubic-bezier(.68,-0.01,.37,1) infinite;
+}
+
+.stars :nth-child(2) {
+  animation: rotate 6s 0.1s cubic-bezier(.68,-0.01,.37,1) infinite;
+
+}
+
+.stars :nth-child(3) {
+  animation: rotate 6s 0.2s cubic-bezier(.68,-0.01,.37,1) infinite;
+
+}
+
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0);
+  }
+
+  20% {
+    transform: rotate(360deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
--- app/components/project/navigation.vue
+++ app/components/project/navigation.vue
@@ -8,75 +8,55 @@ const type = fullPath.includes(WEB) ? WEB : PHONE
 const { project } = useRoute().params
 const path = `/${type}/${project}`
 
-await useAsyncData('prevnext', () => queryContent(type)
+await queryContent(type)
     .findSurround(path)
     .then(res => {
         previous = res[0]
         next = res[1]
-    }))
+    })
 </script>
 
 <template>
     <div class="navigation">
-        <SelectBrackets class="prev" v-if="previous" :to="{ params: { project: previous.name } }">
-            <div class="link-wrapper">
+        <div v-if="previous" class="prev">
+            <SelectBrackets :to="{ params: { project: previous.name } }">
                 <Icon name="tabler:arrow-left-bar" />
-                <h2> {{ previous.name }}</h2>
-                <!-- <img class="mini tw-w-1/2 xl:tw-w-72" :class="{ 'minismall': type === PHONE }" :src="previous.mini" loading="lazy" alt="project picture"> -->
-                <NuxtImg class="mini tw-w-1/2 xl:tw-w-72" :class="{ 'minismall': type === PHONE }" :src="previous.mini"
-                    loading="lazy" alt="project picture" />
-            </div>
-        </SelectBrackets>
-        <SelectBrackets class="next" v-if="next" :to="{ params: { project: next.name } }">
-            <div class="link-wrapper">
-                <NuxtImg class="mini tw-w-1/2 xl:tw-w-72" :class="{ 'minismall': type === PHONE }" :src="next.mini"
-                    loading="lazy" alt="project picture" />
-                <!-- <img class="mini tw-w-1/2 xl:tw-w-72" :class="{ 'minismall': type === PHONE }" :src="next.mini" loading="lazy" alt="project picture"> -->
+                <h2>{{ previous.name }}</h2>
+            </SelectBrackets>
+        </div>
+        <div v-if="next" class="next">
+            <SelectBrackets :to="{ params: { project: next.name } }">
                 <h2>{{ next.name }}</h2>
                 <Icon name="tabler:arrow-right-bar" />
-            </div>
-        </SelectBrackets>
+            </SelectBrackets>
+
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
 .navigation {
-    margin-top: auto;
-
     gap: 0.5rem;
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     grid-template-rows: auto;
-    // display: flex;
-    // flex-flow: column;    
-
-    a {
-        display: flex;
-    }
 }
 
 .prev {
     justify-self: flex-start;
-
-    .link-wrapper {
-        padding-left: 1rem;
-    }
 }
 
 .next {
     grid-column: 2;
     justify-self: flex-end;
-
-    .link-wrapper {
-        padding-right: 1rem;
-    }
 }
 
 .prev,
 .next {
-    cursor: pointer;
+    padding: 1rem;
+    border-radius: 0.5rem;
+    background-color: var(--bg-darker0);
     width: fit-content;
-    // width: 100%;
     height: 100%;
 
     &:hover .mini {
