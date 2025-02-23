5b28d3dfa5f794dc427c18b3e76eab4ff44e9f60
calamina <gbelliere@gmail.com>
2025-02-23 23:01:42 +0100

    add timeline / 404 / project init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
+import * as compiler from "vue/compiler-sfc";
+
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue({ compiler }), mdx()]
 });
