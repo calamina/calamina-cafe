78819da230fce8278b6bb450b4dc8efe239d432a
calamina <gbelliere@gmail.com>
2025-03-03 19:52:25 +0100

    fix project grid !!

--- src/components/projects/ProjectsGrid.vue
+++ src/components/projects/ProjectsGrid.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { Project } from '../../models/Project';
-import { computed, ref } from 'vue';
-import { throttledWatch, useMouse } from '@vueuse/core'
+import { ref } from 'vue';
+import IconImg from '../IconImg.vue';
 
 const { projects, type } = defineProps<{
   projects: Project[]
@@ -15,35 +15,9 @@ const images = Object.entries(
     ? import.meta.glob<{ eager: true }>("/src/assets/img/content/projects/web/*.avif")
     : import.meta.glob<{ eager: true }>("/src/assets/img/content/projects/phone/*.avif")
 );
-const selectedImage = computed(() => images
-  .filter((lel) => lel[0].includes(selectedProject.value ?? 'NOT_FOUND')).map(test => test[0])[0])
 
-const { x, y, sourceType } = useMouse()
-
-// const getImg = (name: string) => images
-//   .filter((lel) => lel[0].includes(name)).map(test => test[0])[0]
-
-throttledWatch(
-  [x, y],
-  ([x, y]) => {
-    if (!selected.value) return
-    const maxX = document.body.getBoundingClientRect().width
-    const maxY = document.body.getBoundingClientRect().height
-    const width = selected.value.getBoundingClientRect().width
-    const height = selected.value.getBoundingClientRect().height
-
-    if (maxX - x > 20 + width) {
-      selected.value.style.left = (x + 20).toString() + 'px'
-    }
-    if (maxY - y > 20 + height) {
-      // selected.value.style.top = (y + 20).toString() + 'px'
-      selected.value.style.top = (y + 20).toString() + 'px'
-    }
-  },
-  { throttle: 20 },
-)
-
-const selected = ref(null as HTMLElement | null);
+const getImg = (name: string) => images
+  .filter((lel) => lel[0].includes(name)).map(test => test[0])[0]
 </script>
 
 <template>
@@ -53,13 +27,22 @@ const selected = ref(null as HTMLElement | null);
       <a class="project" @mouseover="selectedProject = project.name" @mouseout="selectedProject = undefined"
          v-for="project in projects"
          :href="url + project.name">
-        {{ project.name }}
-        <!-- <img :src="getImg(project.name)" alt=""> -->
+
+        <img class="projectImg" :src="getImg(project.name)" alt="">
+        <div class="tags">
+          <p class="tag" v-for="tag of project.tech?.tools">{{ tag }}</p>
+        </div>
+        <div class="info">
+          <p class="name">
+            {{ project.name }}
+            <span class="status" :class="{ 'online': project.online }"></span>
+          </p>
+          <button>
+            <IconImg class="iconImg" name="tabler:dots-diagonal" />
+            <!-- <IconImg class="iconImg" name="tabler:pill" /> -->
+          </button>
+        </div>
       </a>
-    </div>
-    <div class="selected" ref="selected">
-      <!-- <div v-if="!selectedProject">[Select a project]</div> -->
-      <img class="selectedimg" v-if="selectedProject" :key="selectedImage" :src="selectedImage" alt="">
     </div>
   </div>
 </template>
@@ -81,43 +64,119 @@ h2 {
 .projects {
   display: flex;
   flex-flow: column;
-  gap: 0.5rem;
-  // display: grid;
-  // grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-}
+  gap: 1rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
 
-.project {
-  width: 100%;
-  padding: 1.25rem 1rem;
-  background-color: var(--bg-darker0);
-  border-radius: 0.5rem;
-  text-transform: capitalize;
-  align-items: center;
-  // justify-content: center;
-
-  &:hover {
-    background-color: var(--highlight);
+  &:hover>:not(:hover) {
+    // TODO:: test ideas !!
+    // opacity: 0.2;
+    // filter: grayscale(1);
   }
 }
 
-.selected {
-  position: absolute;
+.project {
+  position: relative;
   display: flex;
+  flex-flow: column;
+  width: 100%;
+  gap: 0.25rem;
+  overflow: hidden;
+  border-radius: 0.5rem;
+  // padding: 1.25rem 1rem;
+  // border-radius: 0.5rem;
+  text-transform: capitalize;
   align-items: start;
-  justify-content: start;
-  top: 0;
-  left: 0;
-  max-width: 32rem;
-  height: auto;
-  transition: top 0.1s, left 0.1s;
-  pointer-events: none;
+  // justify-content: center;
+  transition: opacity 0.9s ease-in-out, filter 0.9s ease-in-out;
+
+  &:hover {
+    background-color: inherit;
+    transition: opacity 0.2s ease-in-out, filter 0.2s ease-in-out;
+
+
+    button {
+      background-color: var(--highlight);
+    }
+
+    .iconImg {
+      transform: rotate(180deg);
+    }
+  }
 }
 
-.selectedimg {
+.projectImg {
+  max-height: 20rem;
   width: 100%;
-  max-height: 32rem;
-  object-fit: contain;
-  align-self: flex-start;
+  min-height: 6rem;
   border-radius: 0.5rem;
+  object-fit: cover;
+  flex: 1;
+  overflow: hidden;
+}
+
+.info {
+  width: 100%;
+  display: flex;
+  gap: 0.25rem;
+}
+
+button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 0.5rem;
+  width: 2.5rem;
+  height: auto;
+  background-color: var(--bg-darker0);
+}
+
+.iconImg {
+  transition: transform 0.3s;
+}
+
+.name {
+  padding: 0.25rem 0.5rem;
+  display: block;
+  width: 100%;
+  background-color: var(--bg-darker0);
+  border-radius: 0.5rem;
+  position: relative;
+}
+
+.tags {
+  display: flex;
+  display: none;
+  right: 0;
+  background-color: var(--bg-darker0);
+  border-radius: 0.5rem;
+}
+
+.data {
+  width: 100%;
+  display: flex;
+  align-items: end;
+  gap: 0.25rem;
+}
+
+.test {
+  // background-color: var(--bg-darker0);
+  width: 100%;
+  // height: 0.25rem;
+  border-radius: 0.5rem;
+}
+
+.status {
+  position: absolute;
+  right: 0.25rem;
+  top: 0.25rem;
+  width: 0.5rem;
+  height: 0.5rem;
+  border-radius: 0.5rem;
+  background-color: var(--bg-darker);
+
+  &.online {
+    background-color: var(--highlight);
+  }
 }
 </style>
