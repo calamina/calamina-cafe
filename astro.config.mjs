fc0bc3e5dd2eba8d69bda6db5e9605e8163e4cab
calamina <gbelliere@gmail.com>
2025-03-05 01:44:37 +0100

    test img opti

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,17 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue({ compiler }), mdx()]
+  integrations: [vue({ compiler }), mdx()],
+  image: {
+    domains: ['calamina.cafe', 'senscritique.com'],
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: '**.senscritique.com',
+    },
+    {
+      protocol: 'https',
+      hostname: '**.calamina.cafe',
+    }
+    ],
+  },
 });
