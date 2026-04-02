352ac4d565f6171e48fbb6524c5659cb4b25300e
calamina <gbelliere@gmail.com>
2026-04-02 18:36:46 +0200

    ISO page padding / better content css / better header spacing / ISO titles

--- src/data/description.ts
+++ src/data/description.ts
@@ -2,7 +2,7 @@ export const description = {
   home: "Welcome to calamina cafe. This is the personnal website of developer Grégoire Belliere",
   articles: "Elaborate notes I'm planning to flesh out so they are actually presentable.",
   notes: "Very diverse, personnal, random stuff I want to remember.",
-  accessibility: "Accessibility statement page describing what's in place to make the wabsite accessible and what is planned in the future.",
+  accessibility: "I'm working regurlarly on making this website accessible.",
   privacy: "Privacy statement indicating what information the site saves (close to nothing!)",
   projects: "Personnal projects, including web, design, and more to come (music?)",
 }
