55899769093501d0e9a03ebb5cf708333a906319
calamina <gbelliere@gmail.com>
2026-05-04 20:30:23 +0200

    New title incanvas / visually hidden h1 / better canvas scaling (font & grid) / better canvas overlaying (shadows)

--- src/utils/ascii.ts
+++ src/utils/ascii.ts
@@ -5,7 +5,8 @@ interface Point { x: number, y: number }
 interface MemoryPoint extends Point { age: number }
 
 const container = document.querySelector("#container")
-let SIZE = 24;
+const isMobile = 'ontouchstart' in document.documentElement;
+let SIZE = 22;
 let timer = 0;
 let interval = 250
 let timer2 = 0;
@@ -18,7 +19,6 @@ const clickAnimationDuration = 28
 const history: MemoryPoint[] = []
 const random: MemoryPoint[] = []
 const click: Point = { x: 0, y: 0 }
-const isMobile = 'ontouchstart' in document.documentElement;
 
 const script = (p5: p5) => {
   p5.setMoveThreshold(1)
@@ -27,8 +27,7 @@ const script = (p5: p5) => {
     const canvas = p5.createCanvas(window.innerWidth, window.innerHeight);
     canvas.parent(container);
     const font = await p5.loadFont(apercu);
-    // const font = await p5.loadFont(nectoUrl);
-    p5.textSize(22)
+    p5.textSize(SIZE)
     p5.textFont(font);
     p5.textAlign(p5.CENTER, p5.CENTER);
     p5.background(p5.color("#e1dede"));
@@ -118,10 +117,19 @@ const script = (p5: p5) => {
       clickTimer++
       timer2 = p5.millis();
     }
+
+    drawTitle();
   };
 
-  p5.windowResized = () => p5.resizeCanvas(window.innerWidth, window.innerHeight)
-  p5.mouseMoved = () => history.push({ x: mouseCol, y: mouseRow, age: 1 })
+  p5.windowResized = () => {
+    p5.resizeCanvas(window.innerWidth, window.innerHeight)
+    p5.textSize(SIZE)
+  }
+
+  p5.mouseMoved = () => {
+    history.push({ x: mouseCol, y: mouseRow, age: 1 })
+  }
+
   p5.mouseClicked = () => {
     if (clickTimer < clickAnimationDuration) return
     if (isMobile) {
@@ -222,6 +230,40 @@ const script = (p5: p5) => {
   function getChar(center: Point, point: Point, chars: string = "@#o*. ") {
     const diff = p5.floor(p5.max(p5.abs(center.x - point.x), p5.abs(center.y - point.y)))
     return chars[diff]
+  }
+
+  function drawTitle() {
+    let ratio = 1;
+    if (window.innerWidth < 700) ratio = .5
+    else if (window.innerWidth < 1400) ratio = .75
+    window.innerWidth < 700 ? .5 : 1
+    const s = SIZE * ratio
+    const x = p5.floor(window.innerWidth / SIZE / 2);
+    const y = p5.floor(window.innerHeight / SIZE / 2);
+    const activecolor: p5.Color = p5.color("oklch(80% 0.1 320 / .9)");
+    const discretecolor: p5.Color = p5.color("#00000022");
+
+    const coords = [
+      [-18, -4], [-18, -3], [-18, -2], [-18, 4], [-18, 5], [-18, 6], [-17, -5], [-17, -1], [-17, 3], [-17, 7], [-16, -5], [-16, -1], [-16, 3], [-16, 7], [-15, -4], [-15, -1], [-15, 4], [-15, 7], [-14, -2], [-14, 6], [-13, -5], [-13, -3], [-13, -1], [-13, 3], [-13, 5], [-13, 7], [-12, -5], [-12, -3], [-12, -1], [-12, 3], [-12, 5], [-12, 7], [-11, -4], [-11, -3], [-11, -2], [-11, 4], [-11, 5], [-11, 6], [-10, -1], [-10, 7], [-9, -7], [-9, -6], [-9, -5], [-9, -4], [-9, -3], [-9, -2], [-9, 2], [-9, 3], [-9, 4], [-9, 5], [-9, 6], [-9, 7], [-8, -1], [-8, 1], [-8, 4], [-7, -1], [-7, 1], [-6, -2], [-6, 2], [-6, 4], [-6, 5], [-6, 6], [-5, -5], [-5, -3], [-5, -1], [-5, 3], [-5, 5], [-5, 7], [-4, -5], [-4, -3], [-4, -1], [-4, 3], [-4, 5], [-4, 7], [-3, -4], [-3, -3], [-3, -2], [-3, 4], [-3, 7], [-2, -1],
+      [-1, -4], [-1, -3], [-1, -2], [-1, -1], [0, -5], [1, -5], [2, -4], [2, -3], [2, -2], [2, -1], [3, -5], [4, -5],
+      [5, -4], [5, -3], [5, -2], [6, -1], [7, -6], [7, -4], [7, -3], [7, -2], [7, -1], [9, -4], [9, -3], [9, -2], [9, -1],
+      [10, -5], [11, -5], [12, -4], [12, -3], [12, -2], [13, -1], [14, -2], [14, -1], [15, -5], [15, -3], [15, -1],
+      [16, -5], [16, -3], [16, -1], [17, -4], [17, -3], [17, -2], [18, -1],
+    ]
+
+    p5.fill(activecolor)
+    for (const coord of coords) {
+      p5.square(((x + 1) * SIZE - SIZE / 2) + (coord[0] * s), (y * SIZE - SIZE / 2) + (coord[1] * s), s);
+    }
+
+    const cat = [
+      [-1, 2], [-1, 3], [-1, 4], [-1, 5], [-1, 6], [0, 1], [0, 7], [1, 1], [1, 3], [1, 5], [1, 7], [2, 4], [4, 2], [4, 3], [4, 5], [5, 6], [6, 6], [7, 6], [8, 3], [8, 6], [9, 3], [9, 4], [9, 6], [10, 3], [10, 6], [11, 6], [12, 6], [13, 2], [13, 3], [13, 5], [15, 4], [16, 1], [16, 3], [16, 5], [16, 7], [17, 1], [17, 7], [18, 2], [18, 3], [18, 4], [18, 5], [18, 6]
+    ]
+
+    p5.fill(discretecolor)
+    for (const coord of cat) {
+      p5.square(((x + 1) * SIZE - SIZE / 2) + (coord[0] * s), (y * SIZE - SIZE / 2) + (coord[1] * s), s);
+    }
   }
 }
 
